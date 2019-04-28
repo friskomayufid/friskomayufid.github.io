0c4d2d811b44652d6b7692d5b85673a7bffd6321
Frisko Mayufid <frisko@clappingape.com>
2019-04-29 00:35:47 +0700

    init gatsby

---
+++ src/pages/blog.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { Link, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+import Bio from "../components/bio"
+import SEO from "../components/seo"
+
+export default class blog extends Component {
+  render() {
+    const { data } = this.props
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <div>
+        <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
