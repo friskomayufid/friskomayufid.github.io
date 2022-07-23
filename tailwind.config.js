b0d76c89288b2e22bedabf3b10b1932a7d7b8a70
Frisko Mayufid <friskomayufid@gmail.com>
2022-07-23 10:59:25 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#3D3D3D',
+      'secondary': '#575757',
+      'mainred': '#C31A12',
+      'graywhite': '#E9EBEC',
+      'graysecond': '#D7D7D7',
+      'white': '#FFFFFF'
+    },
+  },
+  plugins: [],
+}
