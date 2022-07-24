ed1f6b5f8fb5c93813be6e3f719bcfe2092f8847
Frisko Mayufid <friskomayufid@gmail.com>
2022-07-24 14:20:52 +0700

    fix error string escape

--- components/About.tsx
+++ components/About.tsx
@@ -5,11 +5,11 @@ function About() {
         About Me
       </h3>
       <p className="text-primary indent-8 text-justify mt-5">
-        Hello, I'm a Programmer and College Student. I specialize in Front end
+        {`Hello, I'm a Programmer and College Student. I specialize in Front end
         web development. Now I'm focus on studying to make fully working
         beautiful applications and websites with codes by learning from the
         internet. I believe there's no limit in this digital era, everyone can
-        study whatever they want. Cheers!
+        study whatever they want. Cheers!`}
       </p>
     </div>
   );
